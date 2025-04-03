0d2d6e287124beac3653086ed1269ea681557d3d
NekoScieuse <nekoscieuse@icloud.com>
2025-04-03 15:51:15 +0200

    solved lab w1d4

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,13 +1,29 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
-
+function maxOfTwoNumbers(maxNum) {
+    return Math.max.apply(null, maxNum);
+}
+const result = ([2], [55])
+console.log(result);
 
 
 
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+    if (words.length === 0) {
+        return null;
+    } else {
+        let longestWord = words[0];
+        for (let i = 0; i < words.length; i++) {
+            if (words[i].length > longestWord.length) {
+                longestWord = words[i];
+            }
+        }
+        return longestWord;
+    }
+}
+console.log(findLongestWord(words));
 
 
 
@@ -15,7 +31,14 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers() {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++){
+        sum += numbers[i];
+    }
+    return sum
+}
+console.log(sumNumbers());
 
 
 
@@ -23,12 +46,27 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers() {
+    let sum = 0;
+    for (let i = 0; i < numbers2.length; i++){
+        sum += numbers2[i];
+    }
+    return sum / numbers2.length
+}
+console.log(averageNumbers());
 
 
 
 
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
-
+function doesWordExist() {
+    if (words2.length === 0) {
+        return null
+    } else {
+        return words2.includes("machine")
+    }
+}
+console.log(doesWordExist());
+// Iteration 6 | Count Repetition       
 function doesWordExist() {}

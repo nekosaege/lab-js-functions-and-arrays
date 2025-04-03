5e78bc98c1c5e922704690231e86d0ac67ff4afd
NekoScieuse <nekoscieuse@icloud.com>
2025-04-03 16:59:02 +0200

    saved lab from week 1 day 4

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,9 +1,16 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers(maxNum) {
-    return Math.max.apply(null, maxNum);
+
+function maxOfTwoNumbers(num1, num2) {
+    if (num1 < num2){
+        return num2
+    }else if (num1 > num2){
+        return num1
+    }else if (num1 = num2){
+        return (num1, num2)
+    }
 }
-const result = ([2], [55])
-console.log(result);
+
+
 
 
 
@@ -31,10 +38,10 @@ console.log(findLongestWord(words));
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {
+function sumNumbers(arr) {
     let sum = 0;
-    for (let i = 0; i < numbers.length; i++){
-        sum += numbers[i];
+    for (let i = 0; i < arr.length; i++){
+        sum += arr[i];
     }
     return sum
 }
@@ -46,27 +53,27 @@ console.log(sumNumbers());
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {
+function averageNumbers(arr) {
     let sum = 0;
-    for (let i = 0; i < numbers2.length; i++){
-        sum += numbers2[i];
+    for (let i = 0; i < arr.length; i++){
+        sum += arr[i];
     }
-    return sum / numbers2.length
+    return sum / arr.length
 }
-console.log(averageNumbers());
+console.log(averageNumbers(arr));
 
 
 
 
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
-function doesWordExist() {
-    if (words2.length === 0) {
+function doesWordExist(arr, word) {
+    if (arr.length === 0) {
         return null
     } else {
-        return words2.includes("machine")
+        return arr.includes(word)
     }
 }
-console.log(doesWordExist());
+console.log(doesWordExist(["machine", "subset", "trouble"], "machine"));
 // Iteration 6 | Count Repetition       
 function doesWordExist() {}
